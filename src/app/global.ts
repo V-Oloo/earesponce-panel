32e365d71d37be8957e1730556f6a0d204f85acb
V-OLOO <oloov534@gmail.com>
2020-04-27 12:43:03 +0300

    added https

--- src/app/global.ts
+++ src/app/global.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  readonly _BaseUri = 'http://dev.reinitresearch.com/api';
+  readonly _BaseUri = 'https://dev.reinitresearch.com/api';
 
 
 }
